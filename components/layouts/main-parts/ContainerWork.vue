12bdff4eba34dcb7fda170532ab9f5a75bb6c559
t4t5u0 <ymmtryk0902@gmail.com>
2020-11-04 22:02:14 +0900

    ファイルを整理した

---
+++ components/layouts/main-parts/ContainerWork.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">this is container</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+    width: 90%;
+    max-width: 1500px;
+    height: 200px;
+    margin-top: 5%;
+    margin-bottom: 5% ;
+    margin-right: auto;
+    margin-left: auto;
+    border: dashed 1px;
+    box-sizing: border-box;
+    
+}
+</style>
