eaeae00ba1725a5b83fbc6a1bf8f327a6b91f256
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-28 22:05:26 +0900

    [add] ヘッダー追加

--- components/layouts/main-parts/TopHeader.vue
+++ components/layouts/main-parts/TopHeader.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>
-    <v-app-bar> t4t5u0's portfolio </v-app-bar>
-  </div>
+  <v-app-bar
+    fixed
+    color="#fff"
+    dark
+    shrink-on-scroll
+    src="https://picsum.photos/1920/1080?random"
+  >
+    <template v-slot:img="{ props }">
+      <v-img
+        v-bind="props"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      ></v-img>
+    </template>
+    <v-app-bar-title>t4t5u0's portfolio</v-app-bar-title>
+    <v-spacer></v-spacer>
+    <v-app-bar-nav-icon> </v-app-bar-nav-icon>
+  </v-app-bar>
 </template>
 
 <script>
