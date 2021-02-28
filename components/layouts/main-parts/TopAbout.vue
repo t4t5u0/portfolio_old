12801abf4405722fa35a089d1937103ccc47c101
t4t5u0 <ymmtryk0902@gmail.com>
2021-02-28 13:58:56 +0900

    [add] aboutのコンテンツを追加

--- components/layouts/main-parts/TopAbout.vue
+++ components/layouts/main-parts/TopAbout.vue
@@ -2,14 +2,90 @@
 // テーブルの中でv-forを回すのが良さそう
 // 
 <template>
+  <div>
+    <h2>About</h2>
+    <v-simple-table>
+      <template v-slot:default>
+        <!-- <thead>
+          <tr>
+            <th>First</th>
+            <th>Second</th>
+          </tr>
+        </thead> -->
+        <tbody>
+          <tr v-for="item in content1" :key="item.key">
+            <td>{{ item.key }}</td>
+            <td>{{ item.value }}</td>
+          </tr>
+          <!-- linkつき -->
+          <tr v-for="item in content2" :key="item.key">
+            <td>{{ item.key }}</td>
+            <td>
+              <a :href="item.link" target="_blank" class="no-margin">{{
+                item.value
+              }}</a>
+            </td>
+          </tr>
+          <!-- skills -->
+          <tr v-for="item in content3" :key="item.key">
+            <td>{{ item.key }}</td>
+            <td>
+              <ul v-for="lang in item.value" :key="lang.id">
+                <li>{{ lang }}</li>
+              </ul>
+            </td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      content1: [
+        {
+          key: "Name",
+          value: "Ryuki Yamamoto (山本 竜生)",
+        },
+        {
+          key: "University",
+          value:
+            "公立はこだて未来大学 情報システム科学部 複雑系知能学科 複雑系コース",
+        },
+      ],
+      content2: [
+        {
+          key: "GitHub",
+          value: "t4t5u0",
+          link: "https://github.com/t4t5u0",
+        },
+        {
+          key: "Qiita",
+          value: "t4t5u0",
+          link: "https://qiita.com/t4t5u0",
+        },
+        {
+          key: "Twitter",
+          value: "@i4mwh4ti4m",
+          link: "https://twitter.com/i4mwh4ti4m",
+        },
+      ],
+      content3: [
+        {
+          key: "Skills",
+          value: ["Python", "Go"],
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
+.no-margin {
+  margin: 0 auto;
+}
 </style>
