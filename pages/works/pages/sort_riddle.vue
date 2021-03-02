ab6d0c37a1047b47524e5988e1ce2ef5344b7061
t4t5u0 <ymmtryk0902@gmail.com>
2021-03-02 23:43:42 +0900

    [add] 雰囲気はこんな感じ

--- pages/works/pages/sort_riddle.vue
+++ pages/works/pages/sort_riddle.vue
@@ -1,13 +1,18 @@
 <template>
-  
+  <div>
+    <TopHeader />
+    <h2>Sort Riddle Bot</h2>
+    <h2>これは何</h2>
+    <h2>作った理由</h2>
+    <h2>技術構成</h2>
+    <h2>使用した技術</h2>
+    <TheFooter />
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
