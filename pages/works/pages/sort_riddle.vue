64d9ea329f40147cbd946d70761b87b7756a66a3
t4t5u0 <ymmtryk0902@gmail.com>
2021-03-02 23:47:53 +0900

    [add] 雑なテキストを追加

--- pages/works/pages/sort_riddle.vue
+++ pages/works/pages/sort_riddle.vue
@@ -3,9 +3,16 @@
     <TopHeader />
     <h2>Sort Riddle Bot</h2>
     <h2>これは何</h2>
+    ソートなぞなぞというゲームをDiscord
+    Botにしました。ソートなぞなぞとは、文字列を文字コード順に並べ替え、並べ替える前の文字列を推測する遊びです。
     <h2>作った理由</h2>
+    TLで見かけたので
+    <h2>使用例</h2>
+    画像
     <h2>技術構成</h2>
+    GCP > docker-compose > python > discord
     <h2>使用した技術</h2>
+    GCP docker-compose python discord bot
     <TheFooter />
   </div>
 </template>
